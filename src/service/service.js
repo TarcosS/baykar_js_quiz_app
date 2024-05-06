328aba80b7b70c2262ca18957dd7f4c7e2163a86
TarcosS <kokkiliculas@gmail.com>
2024-05-06 19:19:49 +0300

    Prettier Formatting && MD File updated

--- src/service/service.js
+++ src/service/service.js
@@ -1,15 +1,16 @@
-import { api } from "../utils/api"
+import { api } from "../utils/api";
 
 const getQuestions = async () => {
-    return await api.get('https://jsonplaceholder.typicode.com/posts')
-        .then(function (response) {
-            return response?.data;
-        })
-        .catch(function (error) {
-            return error;
-        });
-}
+  return await api
+    .get("https://jsonplaceholder.typicode.com/posts")
+    .then(function (response) {
+      return response?.data;
+    })
+    .catch(function (error) {
+      return error;
+    });
+};
 
 export default {
-    getQuestions
-}
+  getQuestions,
+};
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    headers: {
-        'Content-Type': 'application/json',
-    }
-})
-
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
